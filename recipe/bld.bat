ecf668d169e229d8ff32b706662337378d222b33
Eric Larson <larson.eric.d@gmail.com>
2024-12-04 16:55:53 -0500

    FIX: Missed it in win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,9 @@
+:: We are currently building a noarch package, but pysoundfile contains a
+:: install-time check for the OS and injects the sf_wchar_open function
+:: conditionally. If this variable is not set, opening files on Windows doesn't work
+:: Fix coming hopefully with https://github.com/bastibe/python-soundfile/pull/288
+:: Original issue on conda-forge: https://github.com/conda-forge/libsndfile-feedstock/issues/14
+
 echo on
-python -m pip install -v .
+set PYSOUNDFILE_PLATFORM=win32
+%PYTHON% -m pip install . --no-deps -vv
