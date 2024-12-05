99f8fe0960258091c91a1c3ccab22571e118f4b9
Eric Larson <larson.eric.d@gmail.com>
2024-12-05 14:16:39 -0500

    FIX: Restore noarch

--- recipe/bld.bat
+++
@@ -1,9 +0,0 @@
-:: We are currently building a noarch package, but pysoundfile contains a
-:: install-time check for the OS and injects the sf_wchar_open function
-:: conditionally. If this variable is not set, opening files on Windows doesn't work
-:: Fix coming hopefully with https://github.com/bastibe/python-soundfile/pull/288
-:: Original issue on conda-forge: https://github.com/conda-forge/libsndfile-feedstock/issues/14
-
-echo on
-set PYSOUNDFILE_PLATFORM=win32
-%PYTHON% -m pip install . --no-deps -vv
--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-echo "CONDA_BUILD_CROSS_COMPILATION=$CONDA_BUILD_CROSS_COMPILATION"
-echo "build_platform=$build_platform"
-echo "target_platform=$target_platform"  # e.g., linux-aarch64
-set -x
-"${PYTHON}" -m pip install . --no-deps -vv
